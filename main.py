a2ff4a0d21ca9f6ad7ea9ed1811e6ef2a79eed54
Todd Proebsting <proebsting@gmail.com>
2022-09-17 17:33:09 -0700

    subparser

---
+++ main.py
@@ -0,0 +1,86 @@
+import sys
+import json
+import argparse
+
+from grammar import Production, analyze
+from parse_ebnf import Parser
+import gen_random
+import scanner
+import emit
+
+
+def process_grammar(input):
+    lexer = scanner.Scanner(input)
+    p = Parser(lexer)
+    g: list[Production] = p.grammar()
+    state = analyze(g)
+    return g, state
+
+
+def create(args) -> None:
+    if args.input:
+        with open(args.input, "r") as f:
+            input = f.read()
+    else:
+        input = sys.stdin.read()
+    g, state = process_grammar(input)
+    if args.output:
+        with open(args.output, "w") as f:
+            emitter = emit.Emitter(g, state, f, args.verbose)
+            emitter.emit_parser()
+    else:
+        emitter = emit.Emitter(g, state, sys.stdout, args.verbose)
+        emitter.emit_parser()
+
+
+def examples(args) -> None:
+    if args.input:
+        with open(args.input, "r") as f:
+            input = f.read()
+    else:
+        input = sys.stdin.read()
+    g, state = process_grammar(input)
+    L = gen_random.gen_examples(g, state, args.quantity, args.limit)
+    js = json.dumps(L, indent=2) + "\n"
+    if args.output:
+        with open(args.output, "w") as f:
+            f.write(js)
+    else:
+        sys.stdout.write(js)
+
+
+def main():
+    args = parse_args()
+    match args.command:
+        case "create":
+            create(args)
+        case "examples":
+            examples(args)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    create = subparsers.add_parser("create", help="create a parser")
+    create.add_argument("--input", type=str, help="input file")
+    create.add_argument("--output", type=str, help="output file")
+    create.add_argument("--verbose", action="store_true", help="verbose output")
+
+    examples = subparsers.add_parser(
+        "examples", help="create a JSON file with example sentences"
+    )
+    examples.add_argument("--input", type=str, help="input file")
+    examples.add_argument("--output", type=str, help="output file")
+    examples.add_argument(
+        "--quantity", type=int, default=1, help="quantity of output needed"
+    )
+    examples.add_argument(
+        "--limit", type=int, default=100, help="limit on number of iterations"
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
