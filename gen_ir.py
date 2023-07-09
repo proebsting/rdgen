ddf8b7411ca4d316ab436ba13aabd2a25eddd82b
Todd Proebsting <proebsting@gmail.com>
2023-07-09 11:56:45 -0700

    refactoring

--- gen_ir.py
+++ gen_ir.py
@@ -18,7 +18,6 @@ from grammar import (
     Seq0,
     Value,
     Infinite,
-    Target,
     Loop,
 )
 
@@ -45,18 +44,7 @@ class Emitter:
         self.pragmas: Dict[str, Any] = pragmas
         self.verbose: bool = verbose
 
-    def epilogue(self, x: Expr) -> List[ir.Stmt]:
-        stmts: list[ir.Stmt] = []
-        # if x.name and x.target:
-        #     append(stmts, ir.mkCopy(x.target.name, x.name))
-        if x.target:
-            stmts.extend(x.target.side_effect)
-        return stmts
-
     def alts(self, x: Alts) -> List[ir.Stmt]:
-        warnings: list[ir.Warning] = [
-            ir.Warning(w) for w in self.state.warnings[x]
-        ]
         guardeds: List[ir.Guarded] = []
         for v in x.vals:
             guard: ir.Guard = ir.Guard(v.predict)
@@ -64,7 +52,7 @@ class Emitter:
             guardeds.append(ir.Guarded(guard, body))
 
         if_ = ir.SelectAlternative(guardeds, ir.ParseError("syntax error"))
-        return warnings + [if_] + self.epilogue(x)
+        return [if_]
 
     def cons0(
         self, x: Seq0, decls: List[ir.Decl], stmts: List[ir.Stmt]
@@ -75,6 +63,7 @@ class Emitter:
                 decls.append(ir.Decl(x.car.name))
             stmts.extend(car)
             x = x.cdr
+        assert isinstance(x, Lambda)
 
     def sequence(
         self, x: Sequence, tmps: Optional[List[str]] = None
@@ -83,7 +72,7 @@ class Emitter:
         stmts: List[ir.Stmt] = []
         self.cons0(x.seq, decls, stmts)
         seq = ir.Sequence(decls, stmts)
-        return [seq] + self.epilogue(x)
+        return [seq]
 
     def value(self, x: Value) -> List[ir.Stmt]:
         if x.target:
@@ -91,23 +80,18 @@ class Emitter:
         else:
             return [ir.Corn(x.value)]
 
-    def loop_simple(self, target: Optional[Target], simple: bool) -> ir.Stmt:
-        if target and not simple:
-            return ir.AssignEmptyList(target.name)
-        else:
-            return ir.Empty()
-
     def loop_shared(
         self, x: Loop, before: Optional[ir.Guard], after: Optional[ir.Guard]
     ) -> List[ir.Stmt]:
-        warnings: list[ir.Warning] = [
-            ir.Warning(w) for w in self.state.warnings[x]
-        ]
-        init = self.loop_simple(x.target, x.simple)
+        init: List[ir.Stmt] = (
+            [ir.AssignEmptyList(x.target.name)]
+            if x.target and not x.simple
+            else []
+        )
         tmps: list[str] | None = [x.element] if x.element else None
         body: List[ir.Stmt] = self.sequence(x.val, tmps)
         loop = ir.Loop(before, body, after)
-        return [init] + warnings + [loop] + self.epilogue(x)
+        return init + [loop]
 
     def rep(self, x: Rep) -> List[ir.Stmt]:
         guard: ir.Guard = ir.Guard(x.val.predict)
@@ -127,31 +111,26 @@ class Emitter:
         return [ir.Continue()]
 
     def opt(self, x: Opt) -> List[ir.Stmt]:
-        warnings: list[ir.Warning] = [
-            ir.Warning(w) for w in self.state.warnings[x]
-        ]
+        init: List[ir.Stmt]
         if x.target and not x.simple:
-            init = ir.AssignNull(x.target.name)
+            init = [ir.AssignNull(x.target.name)]
         else:
-            init = ir.Empty()
+            init = [ir.Empty()]
 
         guardeds: List[ir.Guarded] = [
             ir.Guarded(ir.Guard(x.val.predict), self.expr(x.val))
         ]
         if_ = ir.SelectAlternative(guardeds, None)
-        return [init] + warnings + [if_] + self.epilogue(x)
+        return init + [if_]
 
     def sym(self, x: Sym) -> List[ir.Stmt]:
-        s: ir.Stmt
         if x.value in (self.state.terms):
-            s = ir.Terminal((x.target and x.target.name), x.value)
+            return [ir.Terminal(x.target and x.target.name, x.value)]
         else:
-            s = ir.NonTerminal((x.target and x.target.name), x.value)
-        return [s] + self.epilogue(x)
+            return [ir.NonTerminal(x.target and x.target.name, x.value)]
 
     def parens(self, x: Parens) -> List[ir.Stmt]:
-        p: List[ir.Stmt] = self.expr(x.e)
-        return p + self.epilogue(x)
+        return self.expr(x.e)
 
     def lambda_(self, x: Lambda) -> List[ir.Stmt]:
         return []
@@ -185,10 +164,15 @@ class Emitter:
                 retval = self.infinite(e)
             case Expr():
                 raise Exception(f"Expr not implemented {e}")
+
+        warnings: list[ir.Warning] = [
+            ir.Warning(w) for w in self.state.warnings[e]
+        ]
         verbose: list[ir.Verbose] = [
             ir.Verbose(e.dump0()),
         ]
-        return verbose + retval
+        epilogue = e.target.side_effect if e.target else []
+        return verbose + warnings + retval + epilogue
 
     def prod(self, p: Production, state: State) -> ir.Function:
         preamble: list[ir.Stmt] = [
